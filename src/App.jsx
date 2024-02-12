f0d84754493c57e15b3051405e24f8cdb5010184
devTee <68411886+oyetunji26@users.noreply.github.com>
2024-02-12 23:02:10 +0100

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -14,20 +14,19 @@ import LoginPage from "./sections/LoginPage";
 import Dashboard from "./sections/Dashboard";
 >>>>>>> 141576d2ea81f9d68287b34c24b5f99fc274118e
 import { Routes, Route } from "react-router-dom";
-import { ParallaxProvider } from "react-scroll-parallax";
 
 function App() {
   return (
     <>
 <<<<<<< HEAD
-      <ParallaxProvider>
+      
         <Routes>
           {/* Will have to change the signup path later */}
           <Route exact path="/" element={<LandingPage />} />
           <Route path="/signup" element={<SignupPage />} />
           <Route path="/login" element={<LoginPage />} />
         </Routes>
-      </ParallaxProvider>
+      
 =======
       <Routes>
         {/* Will have to change the signup path later */}
